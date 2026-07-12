98e1d03696275d7a60ddee32ad76971b49d18a82
md-abu-kayser <abu.kayser.official@gmail.com>
2026-07-12 23:58:08 +0600

    feat(probot): add issue opened webhook comment handler

---
+++ 18-code-snippets/probot-app-example/index.js
@@ -0,0 +1,8 @@
+module.exports = (app) => {
+  app.on("issues.opened", async (context) => {
+    const issueComment = context.issue({
+      body: "Thanks for opening this issue!",
+    });
+    await context.octokit.issues.createComment(issueComment);
+  });
+};
